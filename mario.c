1a6c389fb5c88c998df5f536e385cf2ee6fd62e9
Balaiseee <Balaiseee@users.noreply.github.com>
2020-04-20 03:16:41 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -3,19 +3,20 @@
 
 int main(void)
 {
-    int height;
+    int height; //Declare the variable
     do
     {
         height = get_int("Height?"); //Get the height
-    }while(!(height >= 1) || !(height <= 8));
+    }
+    while (!(height >= 1) || !(height <= 8)); //Re ask for the height if value is not between 1 and 8
     {
         for (int i = 1; i <= height; i++)
         {
-            for (int j = height-i-1; j >= 0 ; j--)
+            for (int j = height - i - 1; j >= 0 ; j--) //Loops for the space
             {
                 printf(" ");
             }
-            for (int j = 0 ; j < i ; j++)
+            for (int j = 0 ; j < i ; j++) //Loops for the '#'
             {
                 printf("#");
             }
