a6dc3d0d0e1dc88c3d332206e60f21e48c0c97e3
Balaiseee <Balaiseee@users.noreply.github.com>
2020-04-20 05:40:24 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -1,10 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <math.h>
-int sum_digit(int num)
+
+int sum_digit(int num) //Function to return the sum of the digits of a number
 {
     int sum = 0;
-    while(num != 0)
+    while (num != 0)
     {
         int digit = num % 10;
         num = num / 10;
@@ -12,10 +13,10 @@ int sum_digit(int num)
     }
     return sum;
 }
-int first_digit(long num, int nb)
+int first_digit(long num, int nb) //Function to return the "nb" first digits of a number
 {
-  	int Count = log10(num);
-  	int FirstDigit = num / pow(10, Count-nb+1);
+    int Count = log10(num);
+    int FirstDigit = num / pow(10, Count - nb + 1);
     return FirstDigit;
 }
 
@@ -26,37 +27,78 @@ int main(void)
     int sumOdd = 0;
     int sumEven = 0;
     long numCopy;
-    bool wrote = false;
-    num = get_long("Number:"); //Get the height
+    bool wrote = false; //Boolean to know if a return has been made through a printf
+    num = get_long("Number:");
     numCopy = num;
-    while(num != 0)
+    while (num != 0) //Browse through the digits of the number
     {
-        if (count%2==1)
+        if (count % 2 == 1)
         {
-            sumEven += sum_digit((num%10)*2);
+            sumOdd += sum_digit((num % 10) * 2); //Step 1 : Luhn’s Algorithm
         }
-        if (count%2==0)
+        if (count % 2 == 0)
         {
-            sumOdd += sum_digit(num%10);
+            sumEven += sum_digit(num % 10); //Step 2 : Luhn’s Algorithm
         }
         count++;
         int digit = num % 10;
         num = num / 10;
     }
-    if((sumEven+sumOdd)%10==0)
+    if ((sumEven + sumOdd) % 10 == 0) //Step 3 : Luhn’s Algorithm
     {
-        if (first_digit(numCopy,2) == 34 && round(log10(numCopy)) == 15) {printf("AMEX\n"); wrote = true;}
-        if (first_digit(numCopy,2) == 37 && round(log10(numCopy)) == 15) {printf("AMEX\n"); wrote = true;}
-        if (first_digit(numCopy,1) == 4 && (round(log10(numCopy)) == 13 || round(log10(numCopy)) == 16) ) {printf("VISA\n"); wrote = true;}
-        if (first_digit(numCopy,2) == 51 && round(log10(numCopy)) == 16) {printf("MASTERCARD\n"); wrote = true;}
-        if (first_digit(numCopy,2) == 52 && round(log10(numCopy)) == 16) {printf("MASTERCARD\n"); wrote = true;}
-        if (first_digit(numCopy,2) == 53 && round(log10(numCopy)) == 16) {printf("MASTERCARD\n"); wrote = true;}
-        if (first_digit(numCopy,2) == 54 && round(log10(numCopy)) == 16) {printf("MASTERCARD\n"); wrote = true;}
-        if (first_digit(numCopy,2) == 55 && round(log10(numCopy)) == 16) {printf("MASTERCARD\n"); wrote = true;}
-        if (wrote == false) {printf("INVALID\n"); wrote = true;}
-    } else if (wrote == false)
+        //AMEX
+        if (first_digit(numCopy, 2) == 34 && round(log10(numCopy)) == 15)
+        {
+            printf("AMEX\n");
+            wrote = true;
+        }
+        if (first_digit(numCopy, 2) == 37 && round(log10(numCopy)) == 15)
+        {
+            printf("AMEX\n");
+            wrote = true;
+        }
+        //VISA
+        if (first_digit(numCopy, 1) == 4 && (round(log10(numCopy)) == 13 || round(log10(numCopy)) == 16))
+        {
+            printf("VISA\n");
+            wrote = true;
+        }
+        //MASTERCARD
+        if (first_digit(numCopy, 2) == 51 && round(log10(numCopy)) == 16)
+        {
+            printf("MASTERCARD\n");
+            wrote = true;
+        }
+        if (first_digit(numCopy, 2) == 52 && round(log10(numCopy)) == 16)
+        {
+            printf("MASTERCARD\n");
+            wrote = true;
+        }
+        if (first_digit(numCopy, 2) == 53 && round(log10(numCopy)) == 16)
+        {
+            printf("MASTERCARD\n");
+            wrote = true;
+        }
+
+        if (first_digit(numCopy, 2) == 54 && round(log10(numCopy)) == 16)
+        {
+            printf("MASTERCARD\n");
+            wrote = true;
+        }
+        if (first_digit(numCopy, 2) == 55 && round(log10(numCopy)) == 16)
+        {
+            printf("MASTERCARD\n");
+            wrote = true;
+        }
+        //INVALID
+        if (wrote == false)
+        {
+            printf("INVALID\n");
+            wrote = true;
+        }
+    }
+    else if (wrote == false)
     {
         printf("INVALID\n");
     }
-
 }
