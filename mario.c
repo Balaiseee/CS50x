893cd878aa35d29a9ce868136caf8b5e0bf87d76
Balaiseee <Balaiseee@users.noreply.github.com>
2020-04-20 02:47:10 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     int height = get_int("Height?"); //Get the height
-    if(!(height <= 0))
+    if((height >= 1) && (height <= 8))
     {
         for (int i = 1; i <= height; i++)
         {
