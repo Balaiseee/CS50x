bec310bedb38479c74ec8888f7841cf3e3a36beb
Balaiseee <Balaiseee@users.noreply.github.com>
2020-04-22 11:17:11 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -1,40 +1,39 @@
-#include <stdio.h>
-#include <cs50.h>
-#include <string.h>
-#include <ctype.h>
-#include <math.h>
+#include <stdio.h> //printf
+#include <cs50.h> //get_string
+#include <string.h> //strlen
+#include <ctype.h> //isalpha
+#include <math.h> //round
 
-int wordCounting (string text);
-int letterCounting (string text);
-int sentenceCounting (string text);
-int index (float letters, float sentences, float words);
+int wordCounting(string text); //Function that returns the number of words in a string
+int letterCounting(string text);//Function that returns the number of letters in a string
+int sentenceCounting(string text);//Function that returns the number of sentences in a string
+int index(float letters, float sentences, float words); //Function that returns the grade of the Coleman-Liau index of a string
 
-int main (void) {
+int main(void)
+{
     string text = get_string("Text : ");
     int letters = 0;
-    //printf("%s\n", text);
-    //printf("%i letter(s) \n", letterCounting(text));
-    //printf("%i word(s) \n", wordCounting(text));
-    //printf("%i sentence(s) \n", sentenceCounting(text));
-    int grade = index(letterCounting(text),sentenceCounting(text), wordCounting(text));
+    int grade = index(letterCounting(text), sentenceCounting(text), wordCounting(text));
     if (grade < 1)
     {
         printf("Before Grade 1\n");
-    } else if (grade > 16)
+    }
+    else if (grade > 16)
     {
         printf("Grade 16+\n");
-    } else
+    }
+    else
     {
         printf("Grade %i\n", grade);
     }
 }
 
-int letterCounting (string text)
+int letterCounting(string text)
 {
     int count = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if(isalpha(text[i]))
+        if (isalpha(text[i])) //Check if the char text[i] is an alphanumeric char
         {
             count++;
         }
@@ -42,12 +41,12 @@ int letterCounting (string text)
     return count;
 }
 
-int wordCounting (string text)
+int wordCounting(string text)
 {
-    int count = 1; //Because the text start with a word not a space
+    int count = 1; //Because the text never end with a space
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if(text[i] == ' ')
+        if (text[i] == ' ')
         {
             count++;
         }
@@ -55,12 +54,12 @@ int wordCounting (string text)
     return count;
 }
 
-int sentenceCounting (string text)
+int sentenceCounting(string text)
 {
     int count = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if(text[i] == '.' || text[i] == '?')
+        if (text[i] == '.' || text[i] == '?')
         {
             count++;
         }
@@ -70,7 +69,7 @@ int sentenceCounting (string text)
 
 int index(float letters, float sentences, float words)
 {
-    float L = letters/words*100;
-    float S = sentences/words*100;
+    float L = letters / words * 100;
+    float S = sentences / words * 100;
     return round(0.0588 * L - 0.296 * S - 15.8);
 }
