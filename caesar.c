c7ab65be19b2808fa3122115fa19a1b32991d6fa
Balaiseee <Balaiseee@users.noreply.github.com>
2020-04-22 14:42:35 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -1,36 +1,35 @@
 #include <stdio.h> //printf
 #include <cs50.h> //get_string
 #include <string.h> //strlen
-#include <stdlib.h>
-//#include <string.h>
+#include <stdlib.h> //atoi
 
 bool isNumber(string text);
 
 int main(int argc, string argv[])
 {
-    if(argc == 2 && isNumber(argv[1]))
+    if (argc == 2 && isNumber(argv[1])) //Check if there is an argument and if it is a number.
     {
         int key = atoi(argv[1]);
         printf("Key: %i\n", key);
         string plaintext = get_string("plaintext:  ");
         string ciphertext = plaintext;
-        char *min = "abcdefghijklmnopqrstuvwxyz";
-        char *maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char *min = "abcdefghijklmnopqrstuvwxyz"; //Lowercase Alphabet
+        char *maj = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //Uppercase Alphabet
         char *e;
         int index;
-        for (int i = 0, n = strlen(plaintext); i< n; i++)
+        for (int i = 0, n = strlen(plaintext); i < n; i++)
         {
             if ((plaintext[i] >= 'a' && plaintext[i] <= 'z'))
             {
                 e = strchr(min, plaintext[i]);
-                index = (int)(e - min);
-                ciphertext[i] = min[(index+key)%26];
+                index = (int)(e - min); //Get the index of the character in the alphabet
+                ciphertext[i] = min[(index + key) % 26]; //Caesar’s algorithm
             }
             else if ((plaintext[i] >= 'A' && plaintext[i] <= 'Z'))
             {
                 e = strchr(maj, plaintext[i]);
-                index = (int)(e - maj);
-                ciphertext[i] = maj[(index+key)%26];
+                index = (int)(e - maj); //Get the index of the character in the alphabet
+                ciphertext[i] = maj[(index + key) % 26];//Caesar’s algorithm
             }
         }
         printf("ciphertext: %s\n", ciphertext);
@@ -42,15 +41,16 @@ int main(int argc, string argv[])
     }
 }
 
-bool isNumber(string text)
+bool isNumber(string text) //Function that returns if the string is a number
 {
     int j;
     j = strlen(text);
-    while(j--)
+    while (j--)
     {
-        if(text[j] >= '0' && text[j] <= '9')
+        if (text[j] >= '0' && text[j] <= '9')
+        {
             continue;
-
+        }
         return false;
     }
     return true;
