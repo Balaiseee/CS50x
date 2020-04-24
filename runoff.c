69b9c4dec0aec49e334eed9269da44b75e7e858d
Balaiseee <Balaiseee@users.noreply.github.com>
2020-04-24 10:52:46 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -186,7 +186,7 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int max = 0, index = 0;
+    int max = voter_count/2, index = 0;
     for (int i = 0; i < candidate_count; i++)
     {
         if (max < candidates[i].votes)
