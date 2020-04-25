0e32b59e7b21844b98c64fb158c076997cc862e1
Balaiseee <Balaiseee@users.noreply.github.com>
2020-04-25 19:35:39 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -182,42 +182,42 @@ void print_winner(void)
     // TODO
     return;
 }
-void mergesort(int a[],int i,int j)
+void mergesort(int a[], int i, int j)
 {
 	int mid;
 		
-	if(i<j)
+	if(i < j)
 	{
-		mid=(i+j)/2;
-		mergesort(a,i,mid);		//left recursion
-		mergesort(a,mid+1,j);	//right recursion
-		merge(a,i,mid,mid+1,j);	//merging of two sorted sub-arrays
+		mid=(i + j) / 2;
+		mergesort(a, i, mid);		//left recursion
+		mergesort(a, mid + 1, j);	//right recursion
+		merge(a, i, mid, mid + 1, j);	//merging of two sorted sub-arrays
 	}
 }
  
-void merge(int a[],int i1,int j1,int i2,int j2)
+void merge(int a[], int i1, int j1, int i2, int j2)
 {
 	int temp[50];	//array used for merging
-	int i,j,k;
-	i=i1;	//beginning of the first list
-	j=i2;	//beginning of the second list
-	k=0;
+	int i, j, k;
+	i = i1;	//beginning of the first list
+	j = i2;	//beginning of the second list
+	k = 0;
 	
-	while(i<=j1 && j<=j2)	//while elements in both lists
+	while(i <= j1 && j <= j2)	//while elements in both lists
 	{
-		if(a[i]<a[j])
-			temp[k++]=a[i++];
+		if(a[i] < a[j])
+			temp[k++] = a[i++];
 		else
-			temp[k++]=a[j++];
+			temp[k++] = a[j++];
 	}
 	
-	while(i<=j1)	//copy remaining elements of the first list
-		temp[k++]=a[i++];
+	while(i <= j1)	//copy remaining elements of the first list
+		temp[k++] = a[i++];
 		
-	while(j<=j2)	//copy remaining elements of the second list
-		temp[k++]=a[j++];
+	while(j <= j2)	//copy remaining elements of the second list
+		temp[k++] = a[j++];
 		
 	//Transfer elements from temp[] back to a[]
-	for(i=i1,j=0;i<=j2;i++,j++)
-		a[i]=temp[j];
+	for(i = i1, j = 0; i <= j2; i++, j++)
+		a[i] = temp[j];
 }
