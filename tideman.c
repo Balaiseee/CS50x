d1b90fb77c807ae4079dd7d2b6091d69ddb0fa43
Balaiseee <Balaiseee@users.noreply.github.com>
2020-04-25 23:14:03 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -172,7 +172,9 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
+    for(int i = 0; i < pair_count; i++)
+    {
+    }
     return;
 }
 
@@ -185,48 +187,48 @@ void print_winner(void)
 
 void mergesort(int a[], int i, int j)
 {
-int mid;
-if (i < j)
-{
-mid = (i + j) / 2;
-mergesort(a, i, mid);//left recursion
-mergesort(a, mid + 1, j);//right recursion
-merge(a, i, mid, mid + 1, j);//merging of two sorted sub-arrays
-}
+    int mid;
+    if (i < j)
+    {
+        mid = (i + j) / 2;
+        mergesort(a, i, mid);//left recursion
+        mergesort(a, mid + 1, j);//right recursion
+        merge(a, i, mid, mid + 1, j);//merging of two sorted sub-arrays
+    }
 }
 
 void merge(int a[], int i1, int j1, int i2, int j2)
 {
-int temp[50];//array used for merging
-int i, j, k;
-i = i1;//beginning of the first list
-j = i2;//beginning of the second list
-k = 0;
+    int temp[50];//array used for merging
+    int i, j, k;
+    i = i1;//beginning of the first list
+    j = i2;//beginning of the second list
+    k = 0;
 
-while (i <= j1 && j <= j2)//while elements in both lists
-{
-if (a[i] < a[j])
-{
-temp[k++] = a[i++];
-}
-else
-{
-temp[k++] = a[j++];
-}
-}
+    while (i <= j1 && j <= j2)//while elements in both lists
+    {
+        if (a[i] < a[j])
+        {
+            temp[k++] = a[i++];
+        }
+        else
+        {
+            temp[k++] = a[j++];
+        }
+    }
 
-while (i <= j1)//copy remaining elements of the first list
-{
-temp[k++] = a[i++];
-}
+    while (i <= j1)//copy remaining elements of the first list
+    {
+        temp[k++] = a[i++];
+    }
 
-while (j <= j2)//copy remaining elements of the second list
-{
-temp[k++] = a[j++];
-}
+    while (j <= j2)//copy remaining elements of the second list
+    {
+        temp[k++] = a[j++];
+    }
 
-for (i = i1, j = 0; i <= j2; i++, j++)//Transfer elements from temp[] back to a[]
-{
-a[i] = temp[j];
-}
+    for (i = i1, j = 0; i <= j2; i++, j++)//Transfer elements from temp[] back to a[]
+    {
+        a[i] = temp[j];
+    }
 }
