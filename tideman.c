a64060c2194a08a82f2dd9c4dc8d8b5695e7134c
Balaiseee <Balaiseee@users.noreply.github.com>
2020-04-26 23:48:52 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -185,6 +185,10 @@ void lock_pairs(void)
     }
     for (int i = 0; i < pair_count; i++)
     {
+        if (pairs[i].winner == pairs[i].loser)
+        {
+            break;
+        }
         locked[i][i] = false;
         locked[pairs[i].winner][pairs[i].loser] = true;
         locked[pairs[i].loser][pairs[i].winner] = false;
