5e6160446aede523fad896279c982588095b5ec9
Balaiseee <Balaiseee@users.noreply.github.com>
2020-05-06 12:22:05 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -169,13 +169,13 @@ void sort_pairs(void)
             }
         }
     }
-    for (c = 0; c < pair_count/2; c++)
+    /*for (c = 0; c < pair_count/2; c++)
     {
         t          = pairs[c];
         pairs[c]   = pairs[end];
         pairs[end] = t;
         end--;
-    }
+    }*/
     /*for (int i = 0; i < pair_count; i++)
     {
         printf("Pairs[%i] = (w:%i, l:%i)\n", i, pairs[i].winner, pairs[i].loser);
