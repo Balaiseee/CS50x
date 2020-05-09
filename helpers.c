188142bc17cdc0a7bb27ee21262d58ea0a6feadb
Balaiseee <Balaiseee@users.noreply.github.com>
2020-05-09 17:24:27 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -27,9 +27,9 @@ void sepia(int height, int width, RGBTRIPLE pixel[height][width])
             BYTE originalRed = pixel[i][j].rgbtRed;
             BYTE originalGreen = pixel[i][j].rgbtGreen;
             BYTE originalBlue = pixel[i][j].rgbtBlue;
-            pixel[i][j].rgbtRed = round((float)(.393 * originalRed + .769 * originalGreen + .189 * originalBlue));
-            pixel[i][j].rgbtGreen = round((float)(.349 * originalRed + .686 * originalGreen + .168 * originalBlue));
-            pixel[i][j].rgbtBlue = round((float)(.272 * originalRed + .534 * originalGreen + .131 * originalBlue));
+            pixel[i][j].rgbtRed = floor((float)(.393 * originalRed + .769 * originalGreen + .189 * originalBlue));
+            pixel[i][j].rgbtGreen = floor((float)(.349 * originalRed + .686 * originalGreen + .168 * originalBlue));
+            pixel[i][j].rgbtBlue = floor((float)(.272 * originalRed + .534 * originalGreen + .131 * originalBlue));
         }
     }
 }
