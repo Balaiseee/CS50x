d0060c259eac649c56e9658b754660b841f79464
Balaiseee <Balaiseee@users.noreply.github.com>
2020-05-11 01:40:37 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -82,12 +82,21 @@ void blur(int height, int width, RGBTRIPLE pixel[height][width])
 {
     for (int i = 1; i < height-1; i++)
     {
-        for(int j = 1; j < width-1; j++)
+        for(int j = 0; j < width-1; j++)
         {
             RGBTRIPLE average;
+            if (j == 0)
+            {
+                average.rgbtRed = round((float)(pixel[i-1][j].rgbtRed+pixel[i-1][j+1].rgbtRed+pixel[i][j].rgbtRed+pixel[i][j+1].rgbtRed+pixel[i+1][j].rgbtRed+pixel[i+1][j+1].rgbtRed)/6);
+                average.rgbtGreen = round((float)(pixel[i-1][j].rgbtGreen+pixel[i-1][j+1].rgbtGreen+pixel[i][j].rgbtGreen+pixel[i][j+1].rgbtGreen+pixel[i+1][j].rgbtGreen+pixel[i+1][j+1].rgbtGreen)/6);
+                average.rgbtBlue = round((float)(pixel[i-1][j].rgbtBlue+pixel[i-1][j+1].rgbtBlue+pixel[i][j].rgbtBlue+pixel[i][j+1].rgbtBlue+pixel[i+1][j].rgbtBlue+pixel[i+1][j+1].rgbtBlue)/6);
+            }
+            else
+            {
             average.rgbtRed = round((float)(pixel[i-1][j-1].rgbtRed+pixel[i-1][j].rgbtRed+pixel[i-1][j+1].rgbtRed+pixel[i][j-1].rgbtRed+pixel[i][j].rgbtRed+pixel[i][j+1].rgbtRed+pixel[i+1][j-1].rgbtRed+pixel[i+1][j].rgbtRed+pixel[i+1][j+1].rgbtRed)/9);
             average.rgbtGreen = round((float)(pixel[i-1][j-1].rgbtGreen+pixel[i-1][j].rgbtGreen+pixel[i-1][j+1].rgbtGreen+pixel[i][j-1].rgbtGreen+pixel[i][j].rgbtGreen+pixel[i][j+1].rgbtGreen+pixel[i+1][j-1].rgbtGreen+pixel[i+1][j].rgbtGreen+pixel[i+1][j+1].rgbtGreen)/9);
             average.rgbtBlue = round((float)(pixel[i-1][j-1].rgbtBlue+pixel[i-1][j].rgbtBlue+pixel[i-1][j+1].rgbtBlue+pixel[i][j-1].rgbtBlue+pixel[i][j].rgbtBlue+pixel[i][j+1].rgbtBlue+pixel[i+1][j-1].rgbtBlue+pixel[i+1][j].rgbtBlue+pixel[i+1][j+1].rgbtBlue)/9);
+            }
             //pixel[i-1][j-1] = average;
             //pixel[i-1][j]   = average;
             //pixel[i-1][j+1] = average;
