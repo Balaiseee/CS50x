972f94a3bc22839a26659204af773723b039a0f7
Balaiseee <Balaiseee@users.noreply.github.com>
2020-05-11 01:30:42 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -78,7 +78,26 @@ void reflect(int height, int width, RGBTRIPLE pixel[height][width])
 }
 
 // Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width])
+void blur(int height, int width, RGBTRIPLE pixel[height][width])
 {
+    for (int i = 1; i < height-1; i++)
+    {
+        for(int j = 1; j < width-1; j++)
+        {
+            RGBTRIPLE average;
+            average.rgbtRed = round((float)(pixel[i-1][j-1].rgbtRed+pixel[i-1][j].rgbtRed+pixel[i-1][j+1].rgbtRed+pixel[i][j-1].rgbtRed+pixel[i][j].rgbtRed+pixel[i][j+1].rgbtRed+pixel[i+1][j-1].rgbtRed+pixel[i+1][j].rgbtRed+pixel[i+1][j+1].rgbtRed)/9);
+            average.rgbtGreen = round((float)(pixel[i-1][j-1].rgbtGreen+pixel[i-1][j].rgbtGreen+pixel[i-1][j+1].rgbtGreen+pixel[i][j-1].rgbtGreen+pixel[i][j].rgbtGreen+pixel[i][j+1].rgbtGreen+pixel[i+1][j-1].rgbtGreen+pixel[i+1][j].rgbtGreen+pixel[i+1][j+1].rgbtGreen)/9);
+            average.rgbtBlue = round((float)(pixel[i-1][j-1].rgbtBlue+pixel[i-1][j].rgbtBlue+pixel[i-1][j+1].rgbtBlue+pixel[i][j-1].rgbtBlue+pixel[i][j].rgbtBlue+pixel[i][j+1].rgbtBlue+pixel[i+1][j-1].rgbtBlue+pixel[i+1][j].rgbtBlue+pixel[i+1][j+1].rgbtBlue)/9);
+            pixel[i-1][j-1] = average;
+            pixel[i-1][j]   = average;
+            pixel[i-1][j+1] = average;
+            pixel[i][j-1]   = average;
+            pixel[i][j]     = average;
+            pixel[i][j+1]   = average;
+            pixel[i+1][j-1] = average;
+            pixel[i+1][j]   = average;
+            pixel[i+1][j+1] = average;
+        }
+    }
     return;
 }
