caa97721e9922f32c761697c3c4e2a4edc510f4b
Balaiseee <Balaiseee@users.noreply.github.com>
2020-05-14 14:35:08 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define BUFFER_SIZE 512
 int main(int argc, char *argv[])
 {
-    FILE* f = fopen(argv[1], "rb");
-    int first = fgetc(f);
+    FILE* f = fopen(argv[1], "r");
     int count = 0;
-    int sig1 = 0xff;
-    int sig2 = 0xd8;
-    int sig3 = 0xd9;
-    int add1, add2;
+    int i = 0;
+    unsigned char buffer[BUFFER_SIZE];
     char *name = "";
-    while ((first = fgetc(f)) != EOF)
+    while (fread(buffer, BUFFER_SIZE, 1, f) == 1)
     {
-        if (first == 0xff && fgetc(f) == 0xd8 && fgetc(f) == 0xff)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xe0) == 0xe0)
         {
             name =  (char*) malloc(3*(sizeof(int)));
             if(count >= 10)
@@ -24,29 +22,14 @@ int main(int argc, char *argv[])
                 strcat(name, "00");
             }
             sprintf(name, "%s%d.jpg", name, count);
-            //printf("%s\n", name);
+            printf("%s\n", name);
             FILE* picture = fopen(name, "w");
-            fputc(sig1, picture);
-            fputc(sig2, picture);
-            fputc(sig1, picture);
-            fputc(sig3, picture);
-            while(!((add1 = fgetc(f)) == 0xff && (add2 = fgetc(f)) == 0xd9))
-            {
-                fputc(add1, picture);
-                fputc(add2, picture);
-            }
+            fwrite(&buffer, BUFFER_SIZE, 1, picture);
             fclose(picture);
             //printf("first byte = %x\n", (unsigned)first);
             free(name);
             count++;
         }
     }
-    FILE* test = fopen("001.jpg", "r");
-    while ((first = fgetc(test)) != EOF)
-    {
-         printf("first byte = %x\n", (unsigned)first);
-    }
-    /* TODO else read failed, empty file?? */
-    fclose(test);
     fclose(f);
 }
