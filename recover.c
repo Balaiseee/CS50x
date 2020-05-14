3f72d260671ae22be067e31237e06c99b1502880
Balaiseee <Balaiseee@users.noreply.github.com>
2020-05-14 15:20:13 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -5,14 +5,22 @@
 int main(int argc, char *argv[])
 {
     FILE* f = fopen(argv[1], "r");
+    FILE* picture = NULL;
     int count = 0;
-    int i = 0;
     unsigned char buffer[BUFFER_SIZE];
     char *name = "";
+    int picture_found = 0;
     while (fread(buffer, BUFFER_SIZE, 1, f) == 1)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xe0) == 0xe0)
         {
+            if(picture_found == 1)
+            {
+                fclose(picture);
+            } else
+            {
+                picture_found = 1;
+            }
             name =  (char*) malloc(3*(sizeof(int)));
             if(count >= 10)
             {
@@ -22,14 +30,16 @@ int main(int argc, char *argv[])
                 strcat(name, "00");
             }
             sprintf(name, "%s%d.jpg", name, count);
-            printf("%s\n", name);
-            FILE* picture = fopen(name, "w");
-            fwrite(&buffer, BUFFER_SIZE, 1, picture);
-            fclose(picture);
-            //printf("first byte = %x\n", (unsigned)first);
+            picture = fopen(name, "w");
             free(name);
             count++;
         }
+        if (picture_found == 1)
+        {
+            fwrite(&buffer, BUFFER_SIZE, 1, picture);
+        }
     }
+    fclose(picture);
     fclose(f);
+    return 0;
 }
