cbc35494b5ad54fc49ac613e60ec40719be1fa96
Balaiseee <Balaiseee@users.noreply.github.com>
2020-07-26 17:32:51 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -1,5 +1,6 @@
 from cs50 import get_string
 import re
+import math
 # Gets text from user
 text = get_string("Text: ")
 # Finds & counts all sentences (here count the strong punctuation)
@@ -9,7 +10,7 @@ words=len(re.findall(r'(\S+)',text))
 # Finds & counts all letters
 letters = sum([text.count(key) for key in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'])
 # Calculates the Coleman-Liau index
-ColemanLiau_index = round(0.0588 * letters/words*100 - 0.296 * sentences/words*100 - 15.8)
+ColemanLiau_index = ceil(0.0588 * letters/words*100 - 0.296 * sentences/words*100 - 15.8)
 # Manages the display of grades
 if (ColemanLiau_index < 1):
     print("Before Grade 1")
