36667770bd1887111da04c626f25037a90f66c31
Balaiseee <Balaiseee@users.noreply.github.com>
2020-07-26 14:42:02 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -3,16 +3,19 @@ from cs50 import get_int
 
 def main():
     i = get_positive_int()
-    #Manages the display of the pyramid
+    # Manages the display of the pyramid
     for c in range(1, i + 1):
         print((i - c) * " " + c * "#")
 
-#Sanitizes the inputs to get a positive integer
+# Sanitizes the inputs to get a positive integer
+
+
 def get_positive_int():
     while True:
         n = get_int("Height: ")
         if n > 0:
             break
     return n
+
 
 main()
