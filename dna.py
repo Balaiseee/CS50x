2302870d7e9bf6d15de250eb8e2956ec0272575d
Balaiseee <Balaiseee@users.noreply.github.com>
2020-09-08 09:36:30 +0000

    automated commit by submit50

--- dna.py
+++ dna.py
@@ -3,7 +3,7 @@ import csv
 import re
 
 
-# Filters a string to  only contain letters
+# Filters a string to only contain letters
 def letters(input):
     valids = []
     for character in input:
@@ -12,36 +12,36 @@ def letters(input):
     return ''.join(valids)
 
 
-# Handles STR parse and read in array
+# Handles STR parse and reads in array
 def parse():
     sequence = next(csv.reader(open(sys.argv[2])))
     sequencelist = []
     for element in letters(list(sequence)):
         sequencelist.append(element)
-    # Handles database parse and read in dictionnary
+    # Handles database parse and reads in dictionnary
     database = csv.DictReader(open(sys.argv[1]))
     return sequencelist, database
 
 
 def compute():
     (sequence, database) = parse()
-    # Converts list into string
+    # Converts a list into a string
     sequence = ''.join(sequence)
-    # Finds number of consecutive TTTTTTCT sequence
+    # Finds the highest number of TTTTTTCT in a row
     TTTTTTCT = max([len(x) // len("TTTTTTCT") for x in re.findall(r'((?:TTTTTTCT)+)', sequence)], default=0)
-    # Finds number of consecutive AGATC sequence
+    # Finds the highest number of AGATC in a row
     AGATC = max([len(x) // len("AGATC") for x in re.findall(r'((?:AGATC)+)', sequence)], default=0)
-    # Finds number of consecutive TCTAG sequence
+    # Finds the highest number of TCTAG in a row
     TCTAG = max([len(x) // len("TCTAG") for x in re.findall(r'((?:TCTAG)+)', sequence)], default=0)
-    # Finds number of consecutive TATC sequence
+    # Finds the highest number of TATC in a row
     TATC = max([len(x) // len("TATC") for x in re.findall(r'((?:TATC)+)', sequence)], default=0)
-    # Finds number of consecutive AATG sequence
+    # Finds the highest number of AATG in a row
     AATG = max([len(x) // len("AATG") for x in re.findall(r'((?:AATG)+)', sequence)], default=0)
-    # Finds number of consecutive GATA sequence
+    # Finds the highest number of GATA in a row
     GATA = max([len(x) // len("GATA") for x in re.findall(r'((?:GATA)+)', sequence)], default=0)
-    # Finds number of consecutive GAAA sequence
+    # Finds the highest number of GAAA in a row
     GAAA = max([len(x) // len("GAAA") for x in re.findall(r'((?:GAAA)+)', sequence)], default=0)
-    # Finds number of consecutive TCTG sequence
+    # Finds the highest number of TCTG in a row
     TCTG = max([len(x) // len("TCTG") for x in re.findall(r'((?:TCTG)+)', sequence)], default=0)
     for row in database:
         # Handles large database comparison
