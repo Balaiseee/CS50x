70cf2d270d305bb3b66bd738c54d79a4c1ea432e
Balaiseee <Balaiseee@users.noreply.github.com>
2020-09-10 13:17:51 +0000

    automated commit by submit50

--- roster.py
+++ roster.py
@@ -3,9 +3,9 @@ import sys
 import csv
 
 db = SQL("sqlite:///students.db")
-list = db.execute("SELECT * FROM students WHERE house LIKE ? ORDER BY last,first", sys.argv[1])
+list = db.execute("SELECT * FROM students WHERE house LIKE ? ORDER BY last,first ASC", sys.argv[1])
 for row in list:
     if row["middle"] == "NULL":
         print(row["first"] + " " + row["last"] + ", born " + str(row["birth"]))
     else:
-        print(row["first"] + " " + row["last"] + " " + row["last"] + ", born " + str(row["birth"]))
+        print(row["first"] + " " + row["middle"] + " " + row["last"] + ", born " + str(row["birth"]))
