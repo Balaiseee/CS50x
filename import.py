d82f39a26eba96ebe5ec94c71fa78cf8a95661c9
Balaiseee <Balaiseee@users.noreply.github.com>
2020-09-10 13:29:04 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -1,22 +1,24 @@
 from cs50 import SQL
 import sys
 import csv
-#Creates database
+
+
+# Creates database
 open("students.db", "w").close()
 db = SQL("sqlite:///students.db")
-#Creates DictReader
+# Creates DictReader
 reader = csv.DictReader(open(sys.argv[1]))
-#Creates table
+# Creates table
 db.execute("CREATE TABLE students (first TEXT,middle TEXT, last TEXT, house TEXT, birth NUMERIC)")
-#Iterates over CSV file
+# Iterates over CSV file
 for row in reader:
-    #Separates names
+    # Separates names
     name = row["name"].split()
-    #Handles long name
+    # Handles long name
     if (len(name) > 2):
         db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
-            name[0], name[1], name[2], row["house"], row["birth"])
-    #Handles short name
+        name[0], name[1], name[2], row["house"], row["birth"])
+    # Handles short name
     else:
         db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
-            name[0], None, name[1], row["house"], row["birth"])
+        name[0], None, name[1], row["house"], row["birth"])
--- roster.py
+++ roster.py
@@ -1,11 +1,13 @@
 from cs50 import SQL
 import sys
 import csv
-#Loads databse
+
+
+# Loads databse
 db = SQL("sqlite:///students.db")
-#Retrieves the list of students of the school entered by the user
+# Retrieves the list of students of the school entered by the user
 list = db.execute("SELECT * FROM students WHERE house LIKE ? ORDER BY last,first ASC", sys.argv[1])
-#Handles the display
+# Handles the display
 for row in list:
     if row["middle"] == None:
         print(row["first"] + " " + row["last"] + ", born " + str(row["birth"]))
