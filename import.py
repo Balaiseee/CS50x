56f2ace5e7bcddbddb499b4292f1a55afabf8736
Balaiseee <Balaiseee@users.noreply.github.com>
2020-09-10 13:15:18 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -7,8 +7,8 @@ db = SQL("sqlite:///students.db")
 reader = csv.DictReader(open(sys.argv[1]))
 db.execute("CREATE TABLE students (first TEXT,middle TEXT, last TEXT, house TEXT, birth NUMERIC)")
 for row in reader:
-  name = row["name"].split()
-  if (len(name) > 2):
-    db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", name[0], name[1], name[2], row["house"], row["birth"])
-  else :
-    db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", name[0], "NULL", name[1], row["house"], row["birth"])
+    name = row["name"].split()
+    if (len(name) > 2):
+        db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", name[0], name[1], name[2], row["house"], row["birth"])
+    else:
+        db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", name[0], None, name[1], row["house"], row["birth"])
