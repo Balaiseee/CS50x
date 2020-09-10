cc620f04530cb27507f7bd540ffc61b4297ec68b
Balaiseee <Balaiseee@users.noreply.github.com>
2020-09-10 13:27:42 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -1,16 +1,22 @@
 from cs50 import SQL
 import sys
 import csv
-
+#Creates database
 open("students.db", "w").close()
 db = SQL("sqlite:///students.db")
+#Creates DictReader
 reader = csv.DictReader(open(sys.argv[1]))
+#Creates table
 db.execute("CREATE TABLE students (first TEXT,middle TEXT, last TEXT, house TEXT, birth NUMERIC)")
+#Iterates over CSV file
 for row in reader:
+    #Separates names
     name = row["name"].split()
+    #Handles long name
     if (len(name) > 2):
         db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
             name[0], name[1], name[2], row["house"], row["birth"])
+    #Handles short name
     else:
         db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
             name[0], None, name[1], row["house"], row["birth"])
