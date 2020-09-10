9b61f24be630651d18c3dc665e2df3f4655f0a8d
Balaiseee <Balaiseee@users.noreply.github.com>
2020-09-10 13:23:01 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -10,7 +10,7 @@ for row in reader:
     name = row["name"].split()
     if (len(name) > 2):
         db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
-        name[0], name[1], name[2], row["house"], row["birth"])
+            name[0], name[1], name[2], row["house"], row["birth"])
     else:
         db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)",
-        name[0], None, name[1], row["house"], row["birth"])
+            name[0], None, name[1], row["house"], row["birth"])
